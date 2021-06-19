45209216e77729fad78858604693803c7c4d64d3
sayanjitdas <sayanjitgit@gmail.com>
2021-06-19 14:03:42 +0530

    commit to check smtp email sending process django...

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -132,3 +132,12 @@ LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# FOR SMTP
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# DEFAULT_FROM_EMAIL = ''
+# EMAIL_HOST = ''
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
